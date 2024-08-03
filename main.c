7334e3dff16d2829b6f44d08528e48a12affa4d0
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2024-08-02 20:59:33 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (int argc, char** argv) {
+	// 1) open file
+	// 2) read its contents into memmory
+	// 3) interpret them into c
+	// 4) call clang on the c file
+	// 5) output the file
+
+	printf("Hello World\n");
+
+	return 0;
+}
